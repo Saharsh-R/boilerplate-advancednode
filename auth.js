8a530a4620c530344df7e9e5799a5d8056b6bbaa
Saharsh-R <48884865+Saharsh-R@users.noreply.github.com>
2021-04-24 21:54:05 +0530

    update for rept.it

--- auth.js
+++ auth.js
@@ -36,7 +36,7 @@ module.exports = function (app, myDataBase) {
       passport.use(new GitHubStrategy({
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: 'http://localhost:3000/auth/github/callback'
+        callbackURL: 'https://boilerplate-advancednode.saharshr.repl.co/auth/github/callback'
       },
         function(accessToken, refreshToken, profile, cb) {
           //Database logic here with callback containing our user object
